f700081312e3250e5f204a63e303ae51ea6d1d15
walternomas <w.barriosvazquez@gmail.com>
2022-08-14 08:47:54 -0300

    ecmascript modules, import, export, export default

---
+++ react-course/src/Greeting.jsx
@@ -0,0 +1,7 @@
+export function Greeting() {
+  return <h1>👋🏼 Hola Componente de React!</h1>;
+}
+
+export function UserCard() {
+  return <h1>User Card</h1>
+}
--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,34 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting";
+import Product, {Navbar} from "./Product";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Greeting() {
-  const name = "Walter";
-  const married = true;
-  const user = {
-    firstName: "ryan",
-    lastName: "ray",
-  };
-  function add(x, y) {
-    return x + y;
-  }
-  return (
-    <>
-      <h1>
-        👋🏼 Hello, {name}!
-      </h1>
-      <h2>Casado?: {married ? " ✅ " : " ❎ "}{married.toString()}</h2>
-      <h3>La suma es {add(10, 30)}</h3>
-      <hr/>
-      <h1>{JSON.stringify(user)}</h1>
-      <h2>{user.firstName}</h2>
-      <h3>{user.lastName}</h3>
-    </>
-  );
-}
 root.render(
   <div>
-    <Greeting />
+    <Greeting/>
+    <UserCard/>
+    <Product/>
+    <Navbar/>
   </div>
 );
