cfc30a896df3c1759239dcd9a29a21cf2d7699be
walternomas <w.barriosvazquez@gmail.com>
2022-08-14 08:35:28 -0300

    JSX, ternary operator, fragment, JSON.stringify, toString

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Greeting() {
-  return <div>
-    <h1>Este es un componente</h1>
-    <p>lorem 123</p>
-  </div>
+  const name = "Walter";
+  const married = true;
+  const user = {
+    firstName: "ryan",
+    lastName: "ray",
+  };
+  function add(x, y) {
+    return x + y;
+  }
+  return (
+    <>
+      <h1>
+        👋🏼 Hello, {name}!
+      </h1>
+      <h2>Casado?: {married ? " ✅ " : " ❎ "}{married.toString()}</h2>
+      <h3>La suma es {add(10, 30)}</h3>
+      <hr/>
+      <h1>{JSON.stringify(user)}</h1>
+      <h2>{user.firstName}</h2>
+      <h3>{user.lastName}</h3>
+    </>
+  );
 }
-root.render(<div>
-  <Greeting />
-  <Greeting />
-  <Greeting />
-</div>);
+root.render(
+  <div>
+    <Greeting />
+  </div>
+);
