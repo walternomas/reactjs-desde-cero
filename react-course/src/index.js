41c7afef9d667755a0769d879f4216ca5839cad4
walternomas <w.barriosvazquez@gmail.com>
2022-08-14 07:56:58 -0300

    components, self closing tags

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hola React!!!</h1>);
+
+function Greeting() {
+  return <div>
+    <h1>Este es un componente</h1>
+    <p>lorem 123</p>
+  </div>
+}
+root.render(<div>
+  <Greeting />
+  <Greeting />
+  <Greeting />
+</div>);
