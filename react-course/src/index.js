541c7e2475558b4516dfae620965b57da77d06a7
walternomas <w.barriosvazquez@gmail.com>
2022-08-15 20:17:13 -0300

    fetch API, promesas

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Button } from "./Button";
-import { TaskCard } from "./Task";
-import { Saludar } from "./Saludar";
+import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <TaskCard ready={true} />
-    <Saludar />
-    <Button />
-    <form onSubmit={(e)=> {
-      e.preventDefault();
-      alert('enviado');
-    }}>
-      <h1>Registro de Usuarios</h1>
-      <button>Send</button>
-    </form>
+    <Posts />
   </>
 );
