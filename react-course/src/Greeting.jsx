9292fb60873814804afce404726e8651e7f6ae57
walternomas <w.barriosvazquez@gmail.com>
2022-08-15 17:31:13 -0300

    React Props, Destructuring

--- react-course/src/Greeting.jsx
+++ react-course/src/Greeting.jsx
@@ -1,7 +1,20 @@
-export function Greeting() {
-  return <h1>👋🏼 Hola Componente de React!</h1>;
+// export function Greeting(props) {
+//   return <h1>👋🏼 {props.title}! Bienvenido {props.name}</h1>;
+// }
+
+export function Greeting({ title, name = 'User' }) {
+  return <h1>👋🏼 {title}! dice {name}</h1>;
 }
 
-export function UserCard() {
-  return <h1>User Card</h1>
+export function UserCard({name, amount, married, address, greet}) {
+  return <div>
+    <h1>{name}</h1>
+    <p>💲{amount}</p>
+    <p>{married ? 'married' : 'single'}</p>
+    <ul>
+      <li>City: {address.city}</li>
+      <li>Street: {address.street}</li>
+    </ul>
+    <strong>{greet()}</strong>
+  </div>
 }
--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
-import Product, {Navbar} from "./Product";
+import Product, { Navbar } from "./Product";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <div>
-    <Greeting/>
-    <UserCard/>
-    <Product/>
-    <Navbar/>
+    <Greeting title="Hola Mundo" name="Joe" />
+    <Greeting title="Hola React" name="Ryan" />
+    <Greeting title="Hola JSX" />
+    <Greeting title="Hola JavaScript" />
+    <Greeting title="Hola Walter" />
+    <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function () {
+        alert('Hello, React!');
+      }}
+    />
   </div>
 );
