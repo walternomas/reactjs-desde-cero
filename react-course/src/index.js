cff21a482565d561d04d56b05c864c5a9b0ccc10
walternomas <w.barriosvazquez@gmail.com>
2022-08-15 20:56:28 -0300

    Arrys en React

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,13 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Posts } from "./Posts";
-import { TaskCard } from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Joe",
+    image: "https://robohash.org/user2",
+  },
+  {
+    id: 3,
+    name: "Marcos",
+    image: "https://robohash.org/user3",
+  }
+];
+
 root.render(
   <>
-    <Posts />
-    <TaskCard />
+    {users.map((user, index) => {
+      return (
+        <div key={index}>
+          <h1>{user.name}</h1>
+          <img src={user.image} alt={user.name} />
+        </div>
+      );
+    })}
   </>
 );
