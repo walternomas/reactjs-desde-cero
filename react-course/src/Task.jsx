47a44843b3e87f01cf1f4e442c216a465d24d0f1
walternomas <w.barriosvazquez@gmail.com>
2022-08-15 20:33:10 -0300

    módulos de terceros: react-icons

--- react-course/src/Task.jsx
+++ react-course/src/Task.jsx
@@ -1,9 +1,10 @@
+import { FaBeer } from 'react-icons/fa';
 import './Task.css';
 
 export function TaskCard({ ready }) {
   return (
     <div className='card'>
-      <h1 className='title'>Mi primer tarea</h1>
+      <h1 className='title'>Mi primer tarea <FaBeer/></h1>
       <span className={ready ? 'bg-green' : 'bg-red'}>
         {ready ? 'Tarea realizada' : 'Tarea pendiente'}
       </span>
--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Posts";
+import { TaskCard } from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <Posts />
+    <TaskCard />
   </>
 );
