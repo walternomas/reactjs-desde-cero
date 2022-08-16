3a6f667d02d79ee7da6627ff8d07a26ad38a6085
walternomas <w.barriosvazquez@gmail.com>
2022-08-16 07:38:14 -0300

    TailwindCSS

--- react-tasks-app/src/components/TaskForm.jsx
+++ react-tasks-app/src/components/TaskForm.jsx
@@ -17,20 +17,26 @@ function TaskForm() {
     setDescription('');
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <div className='max-w-md mx-auto'>
+
+    <form onSubmit={handleSubmit} className='bg-slate-800 p-10 mb-4'>
+      <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>
       <input
         placeholder="Escribe tu tarea..."
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className='bg-slate-300 p-3 w-full mb-2'
         autoFocus
-      />
+        />
       <textarea
         placeholder='Escribe la descripción de la tarea...'
         onChange={(e) => setDescription(e.target.value)}
+        className='bg-slate-300 p-3 w-full mb-2'
         value={description}
-      ></textarea>
-      <button>Guardar</button>
+        ></textarea>
+      <button className='bg-indigo-500 px-3 py-1 text-white rounded-sm hover:bg-indigo-300'>Guardar</button>
     </form>
+        </div>
   );
 }
 
